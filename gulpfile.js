dc627600629987517041fc79fc1ad42eeaeed2c5
chisimdi <ezeanietochisimdi@gmail.com>
2016-10-04 10:19:27 +0100

    Concatenated gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 var gulp        = require('gulp'),
     gUtil       = require('gulp-util'),
-    coffee      = require('gulp-coffee');
+    coffee      = require('gulp-coffee'),
+    concat      = require('gulp-concat');
 
+var coffeeSources = ['components/coffee/tagline.coffee'],
+    jsSources = [
+        'components/scripts/rclick.js',
+        'components/scripts/pixgrid.js',
+        'components/scripts/tagline.js',
+        'components/scripts/template.js'
+    ];
 
 gulp.task('coffee', function() {
-    gulp.src('components/coffee/tagline.coffee')
+    gulp.src(coffeeSources)
         .pipe(coffee({bare: true})
             .on('error', gUtil.log))
         .pipe(gulp.dest('components/scripts')
     )
+});
+
+gulp.task('js', function() {
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'));
 });
